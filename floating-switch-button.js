889835a7ea5112bc164ae10f60736103898b806a
Josh Salyer <joshsalyer23@gmail.com>
2018-07-27 11:40:30 -0500

    Added some TODO notes and shadow dom referance

--- floating-switch-button.js
+++ floating-switch-button.js
@@ -1,6 +1,26 @@
-export default class FloatingSwitchButton extends HTMLElement {
+class FloatingSwitchButton extends HTMLElement {
   constructor() {
     super();
 
+    this.shadow = this.createShadowRoot();
+
   }
 }
+// TODO: Need to dynamically build the menu items based on the passed in attrs
+
+// TODO: Add conditional positioning of menu items
+
+// TODO: Allow custom styles to be passed into the shadow dom
+
+// TODO: Add conditional positioning of menu (bottom left, top right, etc)
+
+// TODO: Allow dynamic trigger text or image or icon
+
+// TODO: Should maybe find a better name for these class names...not very intuitive
+// but floating-switch-button-container seems way too long
+const template = `
+  <div class="fs-btn-container">
+    <div class="fs-btn">+</div>
+
+  </div>
+`;
